134880f1e451521638bb66f7d2aef60c8d13804a
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2004-07-28 11:47:02 +0000

    Almost Finaliazed declweb.tex.

--- doc/paper/Diag.java
+++ doc/paper/Diag.java
@@ -14,38 +14,31 @@ import java.util.Date;
 class UMLOptions {}
 
 /**
- * @composed 1 has * Group
- * @has - - - Member
- * @has - - - Publication
- * @has - - - Project
- */
-class ResearchCenter {}
-
-/**
- * @has - - - Member
- * @has - - - Publication
- * @has - - - Project
+ * @composed "\n1" " has" "\n*" Group
+ * @has "\n\n1" "\nhas" * Member
+ * @assoc "1..*" has 1 Publication
+ * @assoc "1..*" participates "  1..*" Project
  */
 class Group {}
 
 /**
- * @has - - - Publication
+ * @assoc "1..*" has "  0..*" Publication
  */
 class Member {}
 
 /**
- * @has - - - Publication
+ * @assoc "0..*" has "  0..*" Publication
  */
 class Project {}
 
 class Publication {}
 
 /**
- * @has - - - Member
+ * @assoc 0..* present "  1..*" Member
  */
 class Seminar {}
 
 /**
- * @has - - - Group
+ * @assoc 0..* has "1" Group
  */
 class additional_pages {}
